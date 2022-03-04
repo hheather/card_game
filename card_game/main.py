b924b53f2ce9db6e02e007e213c71a9bbdcd7dfa
heather <hheatherbell@gmail.com>
2022-03-04 13:21:06 +0000

    Implemented main.py to give ability to play card game

---
+++ card_game/main.py
@@ -0,0 +1,12 @@
+from card_game.card import Card
+from card_game.deck import Deck
+from card_game.suit import Suit
+from card_game.rank import Rank
+
+
+if __name__ == "__main__":
+    cards = [Card(suit.value, rank.value) for suit in Suit for rank in Rank]
+    deck = Deck(cards)
+    deck.shuffle()
+    result = deck.draw()
+    print(result)
